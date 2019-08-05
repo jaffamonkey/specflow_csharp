9c5fe9150a1fe0ec6e04ef95f0c4ee691499b835
Lee McGeever <lee.mcgeever@peoplefluent.com>
2019-08-05 19:02:07 +0100

    Add project files.

---
+++ CodeBindings/LoginSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace specflow_csharp.CodeBindings
+{
+    [Binding]
+    public class LoginSteps
+    {
+        private const string base_url = "https://preview.netdimensions.com/preview/servlet/ekp/login?target=%2Fpreview%2Fservlet%2Fekp%2FpageLayout";
+        private IWebDriver driver;
+
+        [Given(@"the user is on the login page")]
+        public void GivenTheUserIsOnTheLoginPage()
+        {
+            driver = new ChromeDriver
+            {
+                Url = base_url
+            };
+        }
+        
+        [When(@"they enter valid admin credentials add")]
+        public void WhenTheyEnterValidAdminCredentialsAdd()
+        {
+            driver.FindElement(By.Id("UID")).SendKeys("netd_lee");
+            driver.FindElement(By.Id("PWD")).SendKeys("12345678");
+            driver.FindElement(By.Name("login")).Click();
+        }
+        
+        [Then(@"they should be taken to the home page")]
+        public void ThenTheyShouldBeTakenToTheHomePage()
+        {
+            Assert.That(driver.FindElement(By.CssSelector("#column_0 > div > div.widget-header.row_0 > span")).Text.Equals("RECENT ACTIVITY"));
+        }
+    }
+}
---
+++ Features/SpecFlowFeature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: Login
+	As an admin user I want to be able to login
+
+@smoke
+Scenario: Admin Login
+	Given the user is on the login page
+	When they enter valid admin credentials add
+	Then they should be taken to the home page
+	 
